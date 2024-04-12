590c60f443c3be797d6718d02a450acccc478878
Khushi1524 <khushitiwari2415@gmail.com>
2024-04-12 14:00:27 +0530

    changed port

--- php/conn.php
+++ php/conn.php
@@ -2,6 +2,6 @@
 define("DB_HOST", "localhost");
 define("DB_USER", "root");
 define("DB_PASSWORD", "");
-define("DB_DATABASE", "yogabliss");
+define("DB_DATABASE", "yogabase");
 define("DB_PORT", "3307");
 ?>
